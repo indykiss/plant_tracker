4e646b512ec3fc406594cd1f296c61dae77790f5
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-10 04:04:08 +0000

    added seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
-travel_list = {
-    "Peru" => {
-      :city => "Cusco"
-    },
-    "Cyrpus" => {
-      :city => "Nicosia"
-    },
-    "Texas" => {
-      :city => "Austin"
-    },
-    "California" => {
-      :city => "LA"
-    },
+plant_list = {
+  "Figgy" => {
+    :name => "Figgy"
+    :type => "Fig tree"
+    :water_needed => "high"
+    :light_needed => "high" },
+
+  "Cyprus" => {
+    :name => "Cyprus"
+    :type => "Rubber plant"
+    :water_needed => "high"
+    :light_needed => "medium" },
+
+  "Bojangles" => {
+    :name => "Bojangles"
+    :type => "Dog"
+    :water_needed => "medium"
+    :light_needed => "medium" },
+
+  "Texas" => {
+    :name => "Texas"
+    :type => "Succulent"
+    :water_needed => "low"
+    :light_needed => "high" }
   }
