55b6a857e3ad8e1795066db6b44b2ad600592cad
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-02 18:35:04 +0000

    added validations to decorations

--- app/controllers/decorations_controller.rb
+++ app/controllers/decorations_controller.rb
@@ -1,51 +1,50 @@
 class DecorationsController < ApplicationController
 
   get "/decorations" do
-  #  if logged_in?
+    if logged_in?
       @decorations = Decoration.all
       erb :"decorations/index"
-  #  else
-    #  redirect "/login"
-  #  end
+    else
+      redirect "/login"
+    end
   end
 
   get "/decorations/new" do
-    #if logged_in?
+    if logged_in?
       erb :"decorations/new"
-    # else
-  #    redirect "/login"
-#    end
+     else
+      redirect "/login"
+    end
   end
 
   post '/decorations' do
     @decoration = Decoration.create(params)
     erb :'decorations/show'
-    #binding.pry
   end
 
   get '/decorations/:id' do
-  #  if logged_in?
+    if logged_in?
       @decoration = Decoration.find(params[:id])
       erb :'decorations/show'
-  #  else
-  #    redirect "/login"
-    #end
+    else
+      redirect "/login"
+    end
   end
 
   get '/decorations/:id/edit' do
-  #  if logged_in?
+    if logged_in?
       @decoration = Decoration.find(params[:id])
 
         erb :'decorations/edit'
 #    elsif logged_in? && @plant.user != current_user
 #      redirect "/decorations"
-  #  else
-  #    redirect "/login"
-  #  end
+    else
+      redirect "/login"
+    end
   end
 
   post '/decorations/:id' do
-    #if logged_in?
+    if logged_in?
     @decoration = Decoration.find(params[:id])
 
     @decoration.update(params.select{|k|k=="dec_name" || k=="color"})
@@ -59,9 +58,9 @@ class DecorationsController < ApplicationController
   #    elsif logged_in? && params[:plant][:name] == ""
   #      redirect "/decorations/#{@plant.id}"
 
-  #    else
-  #      redirect "/login"
-  #    end
+      else
+        redirect "/login"
+      end
   end
 
   patch '/decorations/:id' do
@@ -75,13 +74,13 @@ class DecorationsController < ApplicationController
   end
 
   get '/decorations/:id/delete' do
-  #  if logged_in?
+    if logged_in?
       @decoration = Decoration.find_by(params[:id])
       @decoration.delete
       redirect "/decorations"
-#    else
-#      redirect "/login"
-#    end
+    else
+      redirect "/login"
+    end
   end
 
 
